d893e5b465b23c6e3e30b396bc6aefa091097369
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2023-01-26 18:56:41 +0200

    Update afraid-from-observables.ts

--- afraid-from-observables.ts
+++ afraid-from-observables.ts
@@ -6,75 +6,101 @@ interface Observer {
 }
 
 export function afraidFromObs() {
-  class Observable {
-    constructor(private subscriptionFn: Function) {
-      this.subscriptionFn = subscriptionFn;
-    }
+  interface Observer {
+  next?: Function;
+  error?: Function;
+  complete?: Function;
+}
 
-    /**
-     * The subscribe() method takes an observer and calls the subscription function with it. Note that each call to subscribe() invokes the subscription function again.
-     */
-    subscribe(observer: Observer) {
-      return this.subscriptionFn(observer);
-    }
+class Observable {
 
-    map(projectionFunction) {
-      return new Observable((observer) => {
-        return this.subscribe({
-          next(val) {
-            observer.next(projectionFunction(val));
-          },
-          error(e) {
-            observer.error(e);
-          },
-          complete() {
-            observer.complete();
-          },
-        });
+  constructor(private subscriptionFn: Function) {
+    this.subscriptionFn = subscriptionFn;
+  }
+
+  subscribe(observer: Observer) {
+    return this.subscriptionFn(observer);
+  }
+
+  map(projectionFunction) {
+    return new Observable(observer => {
+      return this.subscribe({
+        next(val) { observer.next(projectionFunction(val)) },
+        error(e) { observer.error(e) },
+        complete() { observer.complete() }
       });
-    } //map
-  } //Observable
-
-  const interval = (milliseconds = 0) => {
-    return new Observable((observer: Observer) => {
-      let count = 0;
-
-      const id = setInterval(() => {
-        observer.next(count++);
-      }, milliseconds);
-
-      return {
-        unsubscribe() {
-          observer.complete();
-          clearInterval(id);
-        },
-      };
     });
-  };
-  const source$ = interval(1000);
-  const observer: Observer = {
-    next(val) {
-      console.log(val);
-    },
-    error(e) {
-      console.error(e);
-    },
-    complete() {
-      console.info('Complete!');
-    },
-  };
+  }
 
-  const subscriptions: any[] = [];
-  /**
-   * When we subscribe to the interval observable we are invoking the subscription function, that executes the native JS setInterval() function and notifies the observer each time it’s invoked.
-   */
-  // subscriptions.push(source$.subscribe(observer));
+  filter(predicate: (value) => boolean) {
+    return new Observable(observer => {
+      return this.subscribe({
+        next(val) {
+          if (predicate(val)) {
+            observer.next(val);
+          }
+        },
+        error(e) { observer.error(e) },
+        complete() { observer.complete() }
+      });
+    });
+  }
 
-  /**
-   * When the source emits a new value, the value first gets to the map() subscription function. Then, after applying the projection function on the value, the map() observable emits the value to the final subscription.
-   */
-  subscriptions.push(source$.map((x) => x * 2).subscribe(observer));
-  setTimeout(() => {
-    subscriptions.forEach((subscription) => subscription.unsubscribe());
-  }, 5000);
+  mergeMap(func: (value) => Observable) {
+    return new Observable(observer => {
+      return this.subscribe({
+        next(outerValue) {
+          func(outerValue).subscribe({
+            next(innerValue) {
+              observer.next(innerValue);
+            }
+          });
+        },
+        error(e) { observer.error(e) },
+        complete() { observer.complete() }
+      });
+    });
+  }
+
+}
+
+
+const of = (...values) => {
+  return new Observable(observer => {
+    values.forEach(v => observer.next(v));
+  });
+}
+
+
+const interval = (milliseconds = 0) => {
+  return new Observable(observer => {
+    let count = 0;
+
+    const id = setInterval(() => {
+      observer.next(count++);
+    }, milliseconds);
+
+    return {
+      unsubscribe() {
+        clearInterval(id);
+      }
+    }
+  });
+}
+
+// const subscription = interval(1000).map(v => `${v}s`).filter(v => v === '1s').subscribe({
+//   next(v) {
+//     console.log(v);
+//   }
+// });
+
+const subscription = interval(1000).mergeMap(v => of(v)).subscribe({
+  next(v) {
+    console.log(v);
+  }
+});
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 3000)
 }
